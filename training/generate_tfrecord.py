86c051bed65b9d6aa9085b4fcf50c41e2ea890e1
omoknooni <hihm0609@gmail.com>
2023-05-07 15:18:10 +0900

    fix: add class label generate_tfrecord

--- training/generate_tfrecord.py
+++ training/generate_tfrecord.py
@@ -23,6 +23,10 @@ FLAGS = flags.FLAGS
 def class_text_to_int(row_label):
     if row_label == 'stickynote':
         return 1
+    elif row_label == 'wowboard-front':
+        return 2
+    elif row_label == 'wowboard-back':
+        return 3
     else:
         return 0
 
